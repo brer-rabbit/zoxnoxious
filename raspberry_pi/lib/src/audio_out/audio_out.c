2ee94af5f502ba56f0b3ca4bc52e8a4181a5e0d6
Kyle Farrell <github@patentinvestor.com>
2022-11-14 10:39:12 -0800

    map for v0.2 with commented v0.3 audio out mapping

--- raspberry_pi/lib/src/audio_out/audio_out.c
+++ raspberry_pi/lib/src/audio_out/audio_out.c
@@ -154,14 +154,16 @@ static const struct midi_program_to_gpio midi_program_to_gpio[] = {
   { 0, port0_addr, 0b01000000, 0b11111111 }, // prog 13 - MIX1 VCA Right
   { 0, port0_addr, 0b00000000, 0b01111111 }, // prog 14 - MIX2 VCA Left
   { 0, port0_addr, 0b10000000, 0b11111111 }, // prog 15 - MIX1 VCA Left
-  { 1, port1_addr, 0b01000000, 0b01111100 }, // prog 16 - card A mix 2
-  { 1, port1_addr, 0b11000000, 0b11111100 }, // prog 16 - card A mix 2
-  { 1, port1_addr, 0b01000001, 0b01111101 }, // prog 16 - card A mix 2
-  { 1, port1_addr, 0b11000001, 0b11111101 }, // prog 16 - card A mix 2
-  { 1, port1_addr, 0b01000010, 0b01111110 }, // prog 16 - card A mix 2
-  { 1, port1_addr, 0b11000010, 0b11111110 }, // prog 16 - card A mix 2
-  { 1, port1_addr, 0b00000000, 0b00111100 } // prog 16 - card A mix 2
+  // V0.2:
+  { 1, port1_addr, 0b00000000, 0b00111100 }, // prog 16 - Mix2 off
+  { 1, port1_addr, 0b00000001, 0b01111101 }, // prog 17 - card C mix 2
+  { 1, port1_addr, 0b10000001, 0b11111101 }, // prog 18 - card D mix 2
+  { 1, port1_addr, 0b00000010, 0b01111110 }, // prog 19 - card E mix 2
+  { 1, port1_addr, 0b10000010, 0b11111110 }, // prog 20 - card F mix 2
+  { 1, port1_addr, 0b00000000, 0b01111100 }, // prog 21 - card A mix 2
+  { 1, port1_addr, 0b10000000, 0b11111100 }, // prog 22 - card B mix 2
   /*
+  V0.3 data:
   { 1, port1_addr, 0b10010000, 0b10011111 }, // prog 16 - card A mix 2
   { 1, port1_addr, 0b10110000, 0b10111111 }, // prog 17 - card B mix 2
   { 1, port1_addr, 0b00010000, 0b00011111 }, // prog 18 - card C mix 2
